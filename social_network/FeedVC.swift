8cf2da1cb562045ae60d668fcd52e911d8cb8186
Saumya Raval <saumyaraval@Saumyas-Mac.local>
2017-05-15 14:40:43 -0700

    Added firsbase and complted UI

---
+++ social_network/FeedVC.swift
@@ -0,0 +1,30 @@
+//
+//  FeedVC.swift
+//  social_network
+//
+//  Created by Saumya Raval on 5/2/17.
+//  Copyright © 2017 ImbaGames. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ social_network/SignInVC.swift
@@ -0,0 +1,20 @@
+//
+//  SignIn.swift
+//  social_network
+//
+//  Created by Saumya Raval on 5/2/17.
+//  Copyright © 2017 ImbaGames. All rights reserved.
+//
+
+import UIKit
+
+class SignIn: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
