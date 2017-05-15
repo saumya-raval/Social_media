07a072e9bd820aa85d9873b141cb1609bb7187a1
Saumya Raval <saumyaraval@Saumyas-Mac.local>
2017-05-15 16:24:40 -0700

    added custom UI for login

---
+++ social_network/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  social_network
+//
+//  Created by Saumya Raval on 5/15/17.
+//  Copyright © 2017 ImbaGames. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ social_network/CustomTextField.swift
@@ -0,0 +1,29 @@
+//
+//  CustomTextField.swift
+//  social_network
+//
+//  Created by Saumya Raval on 5/15/17.
+//  Copyright © 2017 ImbaGames. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
