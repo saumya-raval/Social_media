1b65c6e92806aa81068dece130d422dfd768f5bb
Saumya Raval <saumyaraval@Saumyas-Mac.local>
2017-05-15 22:06:31 -0700

    email auth working

--- social_network/SignInVC.swift
+++ social_network/SignInVC.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import Firebase
 
 class SignIn: UIViewController {
-
+    
+    @IBOutlet weak var emailField: CustomTextField!
+    @IBOutlet weak var passwordField: CustomTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func signInTapped(_ sender: Any) {
+        if let email = emailField.text, let password = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("SAUMYA: User successfully signed in with email")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("SAUMYA: Unable to sign in with email")
+                        } else {
+                            print("SAUMYA: User created successfully with email")
+                        }
+                    })
+                }
+            })
+        }
+    }
+    
+    func firebaseAuth(_ credentials: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
+            if error != nil {
+                print("SAUMYA: Error authenticating - \(error)")
+            } else {
+                print("SAUMYA: Successfully authenticated")
+            }
+        })
+    }
+    
+    
 
 }
 
